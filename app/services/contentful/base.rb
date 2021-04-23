5da3649817081aedf5e87804fdd185b5190fb56e
neeraj gupta <neeraj.gupta@oyorooms.com>
2021-04-23 19:39:55 +0530

    testing out a docker build

--- app/services/contentful/base.rb
+++ app/services/contentful/base.rb
@@ -5,8 +5,8 @@ module Contentful
     attr_accessor :client
     def initialize()
       @client = Contentful::Client.new(
-            space: 'kk2bw5ojx476',
-            access_token: '7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c',
+            space: 'kk2bw5ojx476', #creds to be moved to environment variables in prod servers.
+            access_token: '7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c', #todo creds to be moved from here to ENV variables
             dynamic_entries: :auto,
             resource_mapping: {
               'Asset' => ContentfulImage
